20df80c9ea4f10e1cc1f8a7d50a03bb91c1b0aab
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-16 16:33:53 -0500

    Commented in functions that will be needed in script.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
+// grab id's of various page elements
+var searchedCities = $("#searchedCities");
+var citySearch = $("#citySearch");
+
 // test to check input group
 $("#searchBtn").on("click", function(event){
     event.preventDefault();
-    var city = $("#citySearch").val();
+    var city = citySearch.val();
     console.log(city);
 });
+
+// create function to add searched cities as buttons
+
+// create function to search OpenWeather for the searched city
+
+// create function to create cards out of the 5-day forecast
+
+// create function that changes the displayed city with a button click
