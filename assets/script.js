0ed2107a7bbcc5f45b1baac860ddcdf34a758fb0
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-17 15:53:37 -0500

    Changes made to city list handling and styling.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,25 @@
 // grab id's of various page elements
 var searchedCities = $("#searchedCities");
 var citySearch = $("#citySearch");
+var apiKey = "4e4e3d0562345975cee7799375ccc83a";
 
 // test to check input group
 $("#searchBtn").on("click", function(event){
     event.preventDefault();
     var city = citySearch.val();
     console.log(city);
+});
+
+// searched cities added to page as button
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+    var city = citySearch.val();
+    var newButton = $("<a>");
+    newButton.addClass("list-group-item").addClass("list-group-item-action");
+    newButton.text(city);
+    searchedCities.append(newButton);
+    citySearch.val("");
+    newButton.attr("id", city);
 });
 
 // create function to add searched cities as buttons
