4f98eb92c88f44dcc45c23d16d72837992afaddc
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-18 17:37:49 -0500

    Changing display on click added.

--- assets/script.js
+++ assets/script.js
@@ -8,76 +8,74 @@ var windEle = $("#wind");
 var uvEle = $("#uvp");
 var currentCity = $("#currentCity");
 
-// test to check input group
-$("#searchBtn").on("click", function(event){
-    event.preventDefault();
-    var city = citySearch.val();
-    console.log(city);
-    
-});
-
 // searched cities added to page as button
-$("#searchBtn").on("click", function(event){
-    event.preventDefault();
-    var city = citySearch.val();
-    city=city.trim();
-    var newButton = $("<a>");
-    newButton.addClass("list-group-item").addClass("list-group-item-action");
-    newButton.text(city);
-    searchedCities.append(newButton);
-    citySearch.val("");
-    newButton.attr("id", city);
-    cityWeather(city);
-
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  var city = citySearch.val();
+  city = city.trim();
+  if (city !== ""){var newButton = $("<button>");
+  newButton.addClass("list-group-item").addClass("list-group-item-action");
+  newButton.text(city);
+  newButton.attr("type", "button");
+  searchedCities.append(newButton);
+  citySearch.val("");
+  newButton.attr("id", city);
+  cityWeather(city);}
 });
 
 // create function to handle ajax calls
 function cityWeather(searchTerm) {
-    var search = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey;
+  var search =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchTerm +
+    "&appid=" +
+    apiKey;
 
+  $.ajax({
+    url: search,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // retrieve and set temperature, wind speed and humidity
+    var temperature = (response.main.temp - 273.15) * 1.8 + 32;
+    tempEle.text("Temperature: " + temperature.toFixed(1) + " F");
+    var humidityRel = response.main.humidity;
+    humidEle.text("Humidity: " + humidityRel + "%");
+    var windSpeed = response.wind.speed * 2.237;
+    windEle.text("Wind Speed: " + windSpeed + " MPH");
+
+    // get and set current date
+    console.log(response.dt);
+    var startDate = Date(response.dt);
+    var displayDate = startDate.slice(4, 15);
+    console.log(displayDate);
+
+    currentCity.text(response.name + " (" + displayDate + ")");
+    latitude = response.coord.lat;
+    longitude = response.coord.lon;
+    var uvSearch =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey;
     $.ajax({
-        url: search,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        // retrieve and set temperature, wind speed and humidity
-        var temperature = (response.main.temp - 273.15) * 1.80 + 32;
-        tempEle.text("Temperature: " + temperature.toFixed(1) + " F");
-        var humidityRel = response.main.humidity;
-        humidEle.text("Humidity: " + humidityRel + "%");
-        var windSpeed = response.wind.speed * 2.237;
-        windEle.text("Wind Speed: " + windSpeed + " MPH");
-
-        // get and set current date
-        console.log(response.dt);
-        var startDate = Date(response.dt);
-        var displayDate = startDate.slice(4,15);
-        console.log(displayDate);
-
-        currentCity.text(response.name + " (" + displayDate + ")");
-        latitude = response.coord.lat;
-        longitude = response.coord.lon;
-        var uvSearch = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-        $.ajax({
-            url: uvSearch,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            uvEle.text(response.value);
-            
-
-
-        })
+      url: uvSearch,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      uvEle.text(response.value);
     });
+  });
 }
 
 // create function to create cards out of the 5-day forecast
 
 // create function that changes the displayed city with a button click
-$(".list-group-item-action").on("click", function(event){
-    event.stopPropogation();
-    event.preventDefault();
-    var nextCity = this.attr("id");
-    console.log(nextCity);
-    cityWeather(nextCity);
+$(document).on("click", ".list-group-item", function (event) {
+    event.stopPropagation();
+    var newCity = $(this).attr("id");
+  console.log(newCity);
+  cityWeather(newCity);
 });
